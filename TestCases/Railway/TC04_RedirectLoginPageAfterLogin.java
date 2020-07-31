cc26215c5d0c852d9174151c0c3292facfa2dddd
SenPT <phanthisen5195@gmail.com>
2020-07-31 07:15:41 +0700

    modify TC04

--- TestCases/Railway/TC04_RedirectLoginPageAfterLogin.java
+++ TestCases/Railway/TC04_RedirectLoginPageAfterLogin.java
@@ -1,7 +1,6 @@
 package Railway;
 
 import Constant.Constant;
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,28 +8,31 @@ public class TC04_RedirectLoginPageAfterLogin extends BaseTest {
     @Test
     public void TC04() {
         System.out.println("TC04 - User is redirected to Book ticket page after logging in");
-
-        System.out.println("Step 1: Navigate to QA Railway Website");
+        System.out.println("Step 1: ");
         HomePage homePage = new HomePage();
         homePage.open();
 
-        System.out.println("Step 2: Go to tab Book Ticket");
-        homePage.gotoBookTicket();
+        System.out.println("Step 2: Click tab Book Ticket");
+        BookTicket bookTicket = homePage.gotoBookTicket();
+        LoginPage login = new LoginPage();
 
-        System.out.println("Step 3: Login with valid account ");
-        LoginPage loginPage = homePage.gotoLoginPage();
-        loginPage.gotoLoginPage();
-        String actualMes = loginPage.login(Constant.USERNAME, Constant.PASSWORD).getlblHomeHeader().getText();
-
-        System.out.println("Verify redirected to Book ticket page after logging in");
-      /*  String actualMes = loginPage.getlblHeaderLoginPage().getText();*/
+        System.out.println("Verify user is directed to Login page");
+        String actualMes = login.getlblHeaderLoginPage().getText();
         Assert.assertEquals(actualMes, "Login Page", "User is not directed to Login page");
 
-        //verify form bookticket is displayed
+        System.out.println("Step 3: Login with valid account");
+        login.login(Constant.USERNAME, Constant.PASSWORD);
+
+        System.out.println("Verify Book ticket page display");
+        String actualMes1 = login.getlblHeaderLoginPage().getText();
+        Assert.assertEquals(actualMes1, "Book ticket", "User is not directed to Book ticket");
+
+        System.out.println("Verify form Book ticket open");
         if (homePage.getFormBookTicket().isDisplayed() == true) {
             Assert.assertEquals("1","1");
         } else {
             Assert.assertEquals("1","2");
         }
+
     }
 }
